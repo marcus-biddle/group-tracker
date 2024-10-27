4a4039068e18b751a6d56b8490fd5382db09c6ac
Mars <marcusbiddle16@gmail.com>
2024-10-27 01:56:29 -0700

    added login button on home

--- client/src/sections/Home/Introduction.jsx
+++ client/src/sections/Home/Introduction.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Text from '../../components/Text'
 import Header from '../../components/Header'
+import { useNavigate } from 'react-router'
+import { isAuthenticated } from '../../helpers/authHelper'
 
 export const Introduction = ({ activityCount, playerCount }) => {
+  const navigate = useNavigate();
   return (
     <div className='text-center  rounded-md pt-16'>
         <Text size='medium' color='mutedText'>
@@ -12,6 +15,11 @@ export const Introduction = ({ activityCount, playerCount }) => {
         <div className='py-6'>
           <Header level="h1" color="primaryText">Create <span className='text-primary'>Move</span>ment</Header>
         </div>
+        {!isAuthenticated() && <div>
+          <button onClick={() => navigate('/auth')} className='bg-[#E53981] w-[55%] border border-[#121212] rounded-md shadow-lg'>
+            LOGIN
+          </button>
+        </div>}
     </div>
   )
 }
